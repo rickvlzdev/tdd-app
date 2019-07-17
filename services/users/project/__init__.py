5bcdeb51bdd723892b641cdb9a5da23a0d5eade8
Rick Veloz <rickvelozdev@gmail.com>
2019-07-17 10:02:52 -0500

    fixed stylistic errors

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,24 +1,24 @@
 import os
-import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def create_app(script_info=None):
-  app = Flask(__name__)
+    app = Flask(__name__)
 
-  app_settings = os.getenv('APP_SETTINGS')  
-  app.config.from_object(app_settings)
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-  db.init_app(app)
+    db.init_app(app)
 
-  from project.api.users import users_blueprint
-  app.register_blueprint(users_blueprint)
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-  @app.shell_context_processor
-  def ctx():
-    return {'app': app, 'db': db}
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
 
-  return app
+    return app
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,21 +1,24 @@
 import os
 
+
 class BaseConfig:
-  """Base configuration"""
-  TESTING = False
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
-  SECRET_KEY = 'my_precious'
+    """Base configuration"""
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
+
 
 class DevelopmentConfig(BaseConfig):
-  """Development configuration"""
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    """Development configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 
 class TestingConfig(BaseConfig):
-  """Development configuration"""
-  TESTING = True
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    """Development configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+
 
 class ProductionConfig(BaseConfig):
-  """Production configuration"""
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-  
+    """Production configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
