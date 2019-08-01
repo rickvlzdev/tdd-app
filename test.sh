9e2fcc0db4ceeef27bdc32977084c421525e58ea
Rick Veloz <rickvelozdev@gmail.com>
2019-07-31 19:24:58 -0500

    add e2e tests

--- test.sh
+++ test.sh
@@ -17,6 +17,12 @@ docker-compose exec client npm run coverage
 inspect $? client
 docker-compose down
 
+docker-compose -f docker-compose-prod.yml up -d --build
+docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
+./node_modules/.bin/cypress run --config baseUrl=http://localhost
+inspect $? client
+docker-compose -f docker-compose-prod.yml down
+
 if [ -n "${fails}" ]; then
   echo "Tests failed: ${fails}"
   exit 1
